ad257e8798d720f15107a631f0afcd063f8051bf
Pedro <pedroalves0409@gmail.com>
2023-09-20 17:22:43 -0300

    refactor(infrastructure): :art: Moving code for their correct module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 module "sagemaker-execution-role" {
-    source = "./sagemaker-execution-role"
+    source = "./sagemaker/sagemaker-execution-role"
     role_name = "sagemaker-role"
     region = "us-east-1"
 }
 
 module "sagemaker-domain" {
-    source = "./sagemaker-domain"
+    source = "./sagemaker/sagemaker-domain"
     region = "us-east-1"
     role_arn = module.sagemaker-execution-role.role_arn
 }
 
 module "sagemaker-user-profile" {
-    source = "./sagemaker-user-profile"
+    source = "./sagemaker/sagemaker-user-profile"
 
     name = "mlops-sagemaker"
 }
